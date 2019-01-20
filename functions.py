ef40a0a844af36ced939e016b2e48a417ba7c512
SUNKARA VENKATA NAVEEN <40927874+nexar3697@users.noreply.github.com>
2019-01-20 18:24:05 +0530

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,13 @@
+# creating Functions example
+#We Assign Genre,Artist,Year for Song
+def Genre():
+	print("Dance/Electronic")
+def Artist():
+	print("DJ Snake")
+def Year():
+	print(2018)
+#Calling The Functions what we assigned
+Genre()
+Artist()
+Year()
+#Successfully assign the values
